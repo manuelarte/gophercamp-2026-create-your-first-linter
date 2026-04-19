3b5ff6c31f37e422b69e6af4cd3b28890316da64
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2026-04-19 11:33:03 +0200

    chore: small fixes (#11)

--- custom-plugin/main.go
+++ custom-plugin/main.go
@@ -6,13 +6,15 @@ const adultAge = 18
 
 func main() {
 	fmt.Println("Insert your age:")
-	var i int
-	_, err := fmt.Scanf("%d", &i)
+
+	i := new(int)
+
+	_, err := fmt.Scanf("%d", i)
 	if err != nil {
 		panic(err)
 	}
 
-	if i >= adultAge {
+	if *i >= adultAge {
 		fmt.Println("You are allowed")
 	} else {
 		fmt.Println("You aren't allowed")

776eadc7fbf026c74b84ec06309845c086de6b53
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2026-04-21 19:33:43 +0200

    chore: using slog in qrcodegenerator

--- qrcodegenerator/main.go
+++ qrcodegenerator/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/yeqown/go-qrcode/v2"
 	"github.com/yeqown/go-qrcode/writer/standard"
 )
+
+var logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 func main() {
 	createQRCode("https://manuelarte.github.io/gophercamp-2026-create-your-first-linter", "qr-slides.jpeg")
@@ -16,18 +20,30 @@ func main() {
 func createQRCode(url string, filename string) {
 	qrc, err := qrcode.New(url)
 	if err != nil {
-		fmt.Printf("could not generate QRCode: %v", err)
+		logger.Error("could not generate QRCode", slog.Any("err", err))
 		return
 	}
 
 	w, err := standard.New(fmt.Sprintf("../slides/public/%s", filename))
 	if err != nil {
-		fmt.Printf("standard.New failed: %v", err)
+		logger.Error("standard.New failed", slog.Any("err", err))
 		return
 	}
+	defer func(w *standard.Writer) {
+		errC := w.Close()
+		if errC != nil {
+			logger.Error("w.Close failed", slog.Any("err", errC))
+		}
+	}(w)
 
 	// save file
 	if err = qrc.Save(w); err != nil {
-		fmt.Printf("could not save image: %v", err)
+		logger.Error("could not save image", slog.Any("err", err))
+		return
 	}
+	logger.Info(
+		"QR Code generated successfully",
+		slog.String("url", url),
+		slog.String("filename", filename),
+	)
 }

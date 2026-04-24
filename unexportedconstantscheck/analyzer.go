0ae90e8eb0fcfcaa8903b19467a39b8dba9c1dab
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2026-04-24 14:32:13 +0200

    chore: applying feedback gophercamp 2026 (#14)

--- unexportedconstantscheck/analyzer.go
+++ unexportedconstantscheck/analyzer.go
@@ -6,7 +6,6 @@ import (
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
-	"golang.org/x/tools/go/ast/inspector"
 )
 
 func NewAnalyzer() *analysis.Analyzer {
@@ -19,25 +18,25 @@ func NewAnalyzer() *analysis.Analyzer {
 }
 
 func run(pass *analysis.Pass) (any, error) {
-	insp, found := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	if !found {
-		//nolint:nilnil // impossible case.
-		return nil, nil
-	}
+	for _, file := range pass.Files {
+		for _, decl := range file.Decls {
+			genDecl, isGenDecl := decl.(*ast.GenDecl)
+			if !isGenDecl {
+				continue
+			}
 
-	// We are only interested in ValueSpec nodes.
-	nodeFilter := []ast.Node{
-		(*ast.ValueSpec)(nil),
-	}
-
-	insp.Preorder(nodeFilter, func(n ast.Node) {
-		if valueSpec, ok := n.(*ast.ValueSpec); ok {
-			// TODO(manuelarte): to be done
-			//nolint:forbidigo // remove later
-			fmt.Printf("%+v\n", valueSpec)
-			// End TODO(manuelarte)
+			for _, spec := range genDecl.Specs {
+				valueSpec, isValueSpec := spec.(*ast.ValueSpec)
+				if !isValueSpec {
+					continue
+				}
+				// TODO(manuelarte): to be done
+				//nolint:forbidigo // remove later
+				fmt.Printf("%+v\n", valueSpec)
+				// End TODO(manuelarte)
+			}
 		}
-	})
+	}
 
 	//nolint:nilnil //any, error
 	return nil, nil
@@ -55,13 +54,39 @@ func newUnexportedConstantsCheckDiag(i *ast.Ident) analysis.Diagnostic {
 		/* TODO(manuelarte): add later
 		SuggestedFixes: []analysis.SuggestedFix{
 			{
-				Message: "unexported constant %q should be prefixed with _",
+				Message: msg,
 				TextEdits: []analysis.TextEdit{
 					{
 						Pos: i.Pos(),
 						End: i.End(),
 						// TODO(manuelarte): Can someone see the problem of fixing it?
 						NewText: fmt.Appendf(nil, "_%s", i.Name),
+					},
+				},
+			},
+		},
+		*/
+	}
+}
+
+//nolint:unused // to be used later
+func newPrefixedErrConsCheckDiag(i *ast.Ident) analysis.Diagnostic {
+	msg := fmt.Sprintf("unexported err constant %q should not be prefixed with _",
+		i.Name)
+
+	return analysis.Diagnostic{
+		Pos:     i.Pos(),
+		End:     i.End(),
+		Message: msg,
+		/* TODO(manuelarte): add later
+		SuggestedFixes: []analysis.SuggestedFix{
+			{
+				Message: msg,
+				TextEdits: []analysis.TextEdit{
+					{
+						Pos: i.Pos(),
+						End: i.End(),
+						NewText: fmt.Appendf(nil, "%s", i.Name[1:]),
 					},
 				},
 			},
--- unexportedconstantscheck/go.mod
+++ unexportedconstantscheck/go.mod
@@ -1,13 +1,13 @@
 module github.com/manuelarte/gophercamp-2026-create-your-first-linter/unexportedconstantscheck
 
-go 1.24.0
+go 1.26.0
 
 require (
 	github.com/golangci/plugin-module-register v0.1.2
-	golang.org/x/tools v0.42.0
+	golang.org/x/tools v0.44.0
 )
 
 require (
-	golang.org/x/mod v0.33.0 // indirect
-	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/mod v0.35.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
 )

373f6a3ecb6edc8ccd4720f081bafa7fb787b25e
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2026-04-24 14:12:47 +0200

    chore: adding more cases _err

--- unexportedconstantscheck/testdata/src/simple/main.go
+++ unexportedconstantscheck/testdata/src/simple/main.go
@@ -6,6 +6,8 @@ const Rate = 0
 
 const errNotFound = "not found"
 
+const _errNotFound = "not found" // want `unexported err constant "_errNotFound" should not be prefixed with _`
+
 const (
 	group        = "group" // want `unexported constant "group" should be prefixed with _`
 	Of           = "Of"
